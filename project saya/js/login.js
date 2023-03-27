6295e4e2f833b7db1bfe2dba30de2c11eae66cf9
Riskiperdana14 <120175911+Riskiperdana14@users.noreply.github.com>
2023-03-27 09:00:24 +0800

    Create login.js

--- project saya/js/login.js
+++ project saya/js/login.js
@@ -1,14 +1,26 @@
 const form = document.getElementById("form");
 const username = document.getElementById("username");
-const pass = document.getElementById("password");
+const password = document.getElementById("password");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const loc_username = localStorage.getItem("username");
-  const loc_password = localStorage.getItem("password");
+  // ambil data user dari local storage
+  const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
 
-  if (username.value == loc_username && pass.value == loc_password) {
+  // cek apakah username dan password kosong atau tidak
+  if (username.value === "" || password.value === "") {
+    alert("Harap isi semua field");
+    return;
+  }
+
+  // cari data user yang sesuai dengan input user
+  const user = storedUsers.find(
+    (u) => u.username === username.value && u.password === password.value
+  );
+
+  // jika user ditemukan, redirect ke halaman utama
+  if (user) {
     alert("Berhasil login");
     window.location.href = "halaman utama.html";
   } else {
